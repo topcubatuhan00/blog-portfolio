440c53a973d7d8bb5a7cfbc6b9dfd163d61080b2
topcubatuhan00 <topcubatuhan00@gmail.com>
2026-01-18 02:59:58 +0300

    CI / CD test

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddServiceExtensions();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddIdentity<User, IdentityRole>(options =>
 {
